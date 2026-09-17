7b9551032fab91c8ccd345ca1aeb9dae34738f7c
evaniyaanna <evaniyaannaaa@gmail.com>
2026-09-17 16:31:41 +0530

    first commit

---
+++ careconnect/src/main/java/com/careconnect/CareconnectApplication.java
@@ -0,0 +1,13 @@
+package com.careconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareconnectApplication.class, args);
+	}
+
+}
